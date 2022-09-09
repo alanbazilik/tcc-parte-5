760ed6c38c49a99db93198745c38399f15e3bd0e
alanbazilik <alangoncalvesbazilio@gmail.com>
2022-09-09 09:44:34 -0300

    atualização nova

--- src/Dao/CidadeDAO.java
+++ src/Dao/CidadeDAO.java
@@ -4,10 +4,138 @@
  */
 package Dao;
 
+import Model.CidadeModel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author alang
  */
 public class CidadeDAO {
+    Connection con;
+    PreparedStatement pgsql;
+    Statement st;
+    ResultSet rs;
+    public ResultSet listacidade(int fk_uf)
+    {
+        ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+        
+        ResultSet rs = null;
+        String sql = "select * from cidade where fk_uf = ?" 
+                                                        + fk_uf;
+        
+        try {
+            st = con.createStatement(ResultSet.CONCUR_UPDATABLE,
+                    ResultSet.TYPE_SCROLL_INSENSITIVE);
+            
+           rs = st.executeQuery(sql);
+                       
+        } catch (SQLException ex) {
+            Logger.getLogger(CidadeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+        
+        
+    }
     
+    
+    
+   public void Inseretelefone(CidadeModel gs)
+    {
+        ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+        
+        String sql = "insert into cidade"
+                + "(nomecidade ,fk_uf)"
+                + " values (?,?)";
+        
+        try {
+            pgsql = con.prepareStatement(sql);
+            pgsql.setString(1, gs.getNomecidade());
+            pgsql.setInt(2, gs.getFk_uf());
+  
+            pgsql.executeUpdate();
+            JOptionPane.showMessageDialog(null, "cidade foi  Cadastrado com Sucesso");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CidadeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void Alteratipotel(CidadeModel gs)
+    {
+        ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+        
+        String sql = "UPDATE cidade SET "
+                + "nomecidade = ?,fk_uf = ?"
+                + "WHERE codcidade = ?";
+        
+        try {
+            pgsql = con.prepareStatement(sql);
+            pgsql.setString(1, gs.getNomecidade());
+            pgsql.setInt(2, gs.getFk_uf());
+            pgsql.setInt(3, gs.getCodcidade());
+   
+            int rowsUpdated = pgsql.executeUpdate();
+            if (rowsUpdated > 0) {
+            JOptionPane.showMessageDialog(null, "A cidade foi alterado com sucesso");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CidadeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+ 
+    
+  public void Excluitipotel(CidadeModel gs)
+    {
+        ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+        
+        String sql ="delete from cidade where"
+                + " codcidade = ?";
+        
+        try {
+            pgsql = con.prepareStatement(sql);
+            pgsql.setInt(1, gs.getCodcidade());
+            pgsql.executeUpdate();
+            JOptionPane.showMessageDialog(null, "cidade foi Excluído com Sucesso");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CidadeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     public ResultSet PesquisarTodosTelefone(String cidade)
+    {
+           ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+       
+        
+        String sql = "select * from cidade where "
+                + "upper(nomecidade) like upper(?)";
+        
+        try {
+            pgsql = con.prepareStatement(sql);
+            pgsql.setString(1,"%" + cidade + "%");
+            
+            st = con.createStatement(ResultSet.CONCUR_UPDATABLE,
+                                     ResultSet.TYPE_SCROLL_INSENSITIVE);
+            
+            rs = st.executeQuery(pgsql.toString());
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CidadeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+        
+        
+    }    
+         
 }
--- src/Dao/ProdutoDAO.java
+++ src/Dao/ProdutoDAO.java
@@ -4,10 +4,166 @@
  */
 package Dao;
 
+import Model.ProdutoModel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author alang
  */
 public class ProdutoDAO {
+      Connection con;
+    PreparedStatement pgsql;
+    Statement st;
+    ResultSet rs;
+    public ResultSet listamarca(int fk_marca)
+    {
+        ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+        
+        ResultSet rs = null;
+        String sql = "select * from produto where fk_marca = ?" 
+                                                        + fk_marca;
+        
+        try {
+            st = con.createStatement(ResultSet.CONCUR_UPDATABLE,
+                    ResultSet.TYPE_SCROLL_INSENSITIVE);
+            
+           rs = st.executeQuery(sql);
+                       
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+        
+        
+    }
+   public ResultSet listatipo(int fk_tipo)
+    {
+        ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+        
+        ResultSet rs = null;
+        String sql = "select * from produto where fk_tipo = ?" 
+                                                        + fk_tipo;
+        
+        try {
+            st = con.createStatement(ResultSet.CONCUR_UPDATABLE,
+                    ResultSet.TYPE_SCROLL_INSENSITIVE);
+            
+           rs = st.executeQuery(sql);
+                       
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+        
+        
+    }    
     
+    
+   public void Inseretelefone(ProdutoModel gs)
+    {
+        ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+        
+        String sql = "insert into produto"
+                + "(produto ,quantidade,fk_tipo,fk_marca)"
+                + " values (upper(?),?,?,?)";
+        
+        try {
+            pgsql = con.prepareStatement(sql);
+            pgsql.setString(1, gs.getProduto());
+            pgsql.setInt(2, gs.getQuantidade());
+             pgsql.setInt(3, gs.getFk_tipo());
+             pgsql.setInt(4, gs.getFk_marca());
+             
+            
+            pgsql.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Produto foi  Cadastrado com Sucesso");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void Alteratipotel(ProdutoModel gs)
+    {
+        ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+        
+        String sql = "UPDATE produto SET "
+                + "produto = ?,quantidade = ?,fk_tipo = ?,fk_marca = ?"
+                + "WHERE codigo_barras = ?";
+        
+        try {
+            pgsql = con.prepareStatement(sql);
+        pgsql.setString(1, gs.getProduto());
+            pgsql.setInt(2, gs.getQuantidade());
+             pgsql.setInt(3, gs.getFk_tipo());
+             pgsql.setInt(4, gs.getFk_marca());
+            pgsql.setInt(5, gs.getCodigo_barras());
+            
+
+            
+            int rowsUpdated = pgsql.executeUpdate();
+            if (rowsUpdated > 0) {
+            JOptionPane.showMessageDialog(null, "O Produto foi alterado com sucesso");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+ 
+    
+  public void Excluitipotel(ProdutoModel gs)
+    {
+        ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+        
+        String sql ="delete from produto where"
+                + " codigo_barras = ?";
+        
+        try {
+            pgsql = con.prepareStatement(sql);
+            pgsql.setInt(1, gs.getCodigo_barras());
+            pgsql.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Produto foi Excluído com Sucesso");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     public ResultSet PesquisarTodosTelefone(String produtos)
+    {
+           ConexaoDAO cb = new ConexaoDAO();
+        con = cb.conectaPostgre();
+       
+        
+        String sql = "select * from produto where "
+                + "upper(produto) like upper(?)";
+        
+        try {
+            pgsql = con.prepareStatement(sql);
+            pgsql.setString(1,"%" + produtos + "%");
+            
+            st = con.createStatement(ResultSet.CONCUR_UPDATABLE,
+                                     ResultSet.TYPE_SCROLL_INSENSITIVE);
+            
+            rs = st.executeQuery(pgsql.toString());
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+        
+        
+    }    
+         
 }
